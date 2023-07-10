0f46592cdbbe9fa089eff6a260485ca994fc5c23
Raphael Borba <raphaelbsx@gmail.com>
2023-07-10 12:02:56 -0300

    initial configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Roboto, Inter } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: ['100', '300', '400', '500', '700'],
+  subsets: ['latin']
+})
+
+const inter = Inter({
+  subsets:['latin']
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${roboto.className}`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
